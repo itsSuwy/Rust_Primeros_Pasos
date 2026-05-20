a76828ddc8d21f33eaa17a03b1b3af96cebbcea6
itsSuwy <diego.velazquez.2006@outlook.es>
2026-05-19 22:49:38 -0600

    The rules to copy a varible inside another variable

--- Strings/src/main.rs
+++ Strings/src/main.rs
@@ -17,6 +17,34 @@ fn main() {
             .read_line(&mut input)// Step 3: Here we read te input from the user
             .expect("Failed to read line"); // Step 4: If something goes wrong, this line saves the code from a crash
 
+    // How to copy a string inside another variable
+    let x: u8 = 9; // This perfectly works!
+    let y: u8 = x;
+    println!("The value of x is {x}");
+    println!("The value of y is {y}");
+
+    // But what about this?
+    let str = String::from("Hello, world!");
+    let stre_1 = str;
+    //println!("{str}"); // This line will crash our program!
+    println!("{stre_1}");
+    // When you have one string and you store it inside another string in that way, rust will liberate the memory from the original string
+    // To copy and have two identical strings, we have to do this
+    let s1 = String::from("Hello, world!");
+    let s2 = s1.clone(); // Copy but dont destroy the original variable
+    println!("{s1} from S1");
+    println!("{s2} from S2");
+
+    // Why does it work with integers but NOT with strings?
+    // Rule: For 'let y = x' to leave BOTH variables alive, the type must implement the COPY trait.
+    // This only applies to types with a FIXED and KNOWN size in bits at compile-time.
+    // STACK: Where types with fixed sizes live (Integers, Unsigned, Floats, Bool, Char).
+    // They are cheap to duplicate, so Rust performs a quick bitwise copy in the Stack.
+    // HEAP: Where dynamic, unknown-size data lives (Strings).
+    // Even though the String pointer is in the Stack, the actual text is in the Heap.
+    // Rust FORBIDS 'Copy' here to prevent pointers from duplicating and messing up hardware memory (Double Free Error).
+
+
     // Bonus: Life of a variable
     let x:u8 = 9;
     {
